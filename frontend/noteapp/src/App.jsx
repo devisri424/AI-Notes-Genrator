d55b32216c64c9f8ed4b1a9a50685c1375683ddb
devisri424 <devisriramasamy24@gmail.com>
2026-05-30 14:30:07 +0530

    updated frontend UI and backend URL

--- frontend/noteapp/src/App.jsx
+++ frontend/noteapp/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setNotes("Generating notes...");
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/generate", {
+      const response = await fetch("https://ai-notes-genrator.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
